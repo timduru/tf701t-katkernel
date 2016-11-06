4b7b79e3dfe9f9b9b5fd380cafc54b58aead70b5
timduru <github@timduru.org>
2016-11-06 10:36:04 +0100

    binder: allow unconditionnal renice

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -516,10 +516,10 @@ static inline void binder_unlock(const char *tag)
 static void binder_set_nice(long nice)
 {
 	long min_nice;
-	if (can_nice(current, nice)) {
+//	if (can_nice(current, nice)) {
 		set_user_nice(current, nice);
 		return;
-	}
+//	}
 	min_nice = 20 - current->signal->rlim[RLIMIT_NICE].rlim_cur;
 	binder_debug(BINDER_DEBUG_PRIORITY_CAP,
 		     "binder: %d: nice value %ld not allowed use "
